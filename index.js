4f59b3995b15f6657af317c96f8f407c06ec0d7f
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-08-12 08:57:29 -0600

    commit tree 42ef35170a43d92e21829fc8a4497209b874f74f

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    console.log(result)
+    return !!result ? result.year : undefined;
+}
